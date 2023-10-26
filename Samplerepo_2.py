3a3c22099f7d18b5196ed283d96b1a72dd487bd1
sandeshpadar <sandeshpadar1@gmail.com>
2023-10-26 07:45:45 +0530

    For my second commit

---
+++ Samplerepo_2.py
@@ -0,0 +1,6 @@
+# how to find out area of tringle
+
+base = float(input('Enter the number of base:- '))
+hight = float(input('Enter the number of hight:- '))
+area = 1.5 * base * hight
+print(f'The area of tringle is {area} square meter')
